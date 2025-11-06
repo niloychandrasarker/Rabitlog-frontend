cdd1a1e67445e95cdd267c604a22e3212fa2c5bd
niloychandrasarker <niloysarker.cs@gmail.com>
2025-11-06 16:56:50 +0600

    sign in and signUp form update

--- src/Routes/LoginPage.jsx
+++ src/Routes/LoginPage.jsx
@@ -1,11 +1,28 @@
-import { SignIn } from "@clerk/clerk-react"
+import { SignIn } from "@clerk/clerk-react";
 
 const LoginPage = () => {
   return (
-    <div className='flex justify-center items-center h-[calc(100vh-70px)]'>
-        <SignIn signUpUrl="/register"/>
+    <div className="flex justify-center items-center h-[calc(100vh-70px)] py-4">
+      <div className="w-full max-w-md max-h-[90vh] overflow-auto">
+        <SignIn
+          signUpUrl="/register"
+          appearance={{
+            elements: {
+              footer: {
+                "& > div:last-child": {
+                  display: "none",
+                },
+              },
+            },
+            layout: {
+              socialButtonsPlacement: "top",
+              showOptionalFields: false,
+            },
+          }}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- src/Routes/RegisterPage.jsx
+++ src/Routes/RegisterPage.jsx
@@ -2,8 +2,25 @@ import { SignUp } from "@clerk/clerk-react";
 
 const RegisterPage = () => {
   return (
-    <div className="flex justify-center items-center h-[calc(100vh-70px)]">
-      <SignUp signInUrl="/login" />
+    <div className="flex justify-center items-center h-[calc(100vh-70px)] py-4">
+      <div className="w-full max-w-md max-h-[80vh] overflow-auto">
+        <SignUp
+          signInUrl="/login"
+          appearance={{
+            elements: {
+              footer: {
+                "& > div:last-child": {
+                  display: "none",
+                },
+              },
+            },
+            layout: {
+              socialButtonsPlacement: "top",
+              showOptionalFields: false,
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
